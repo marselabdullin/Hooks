354ac04aad6583b75fe39b1ac8ff37c444d0cef9
marselabdullin <abdmars@mail.ru>
2019-12-10 22:44:46 +0300

    final

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,11 +8,12 @@ export default function About() {
                     <span className="sr-only">Loading...</span>
                 </div>
             </div>
-            <div class="jumbotron">
+            <div className="jumbotron">
                 <h1 className="display-4">Привет, мир!</h1>
                     <p className="lead">Это простой пример блока с компонентом в стиле jumbotron для привлечения дополнительного внимания к содержанию или информации.</p>
                 <hr className="my-4"/>
                     <p>Использются служебные классы для типографики и расстояния содержимого в контейнере большего размера.</p>
+                    <p>Спиннер не остановиться! :)</p>
             </div>
         </Fragment>
     )
